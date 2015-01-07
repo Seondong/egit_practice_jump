9f620ef35e5c9038466dd5d49335e2253662771c
Seondong <sdkim0211@gmail.com>
2015-01-07 14:47:17 +0900

    First Upload

---
+++ gitprac2/src/gitprac2/jump.java
@@ -0,0 +1,8 @@
+package gitprac2;
+
+public class jump {
+	int a = 10;
+	public static void jumphalo(){
+		System.out.println("JumpJumpEnergy");
+	}
+}
---
+++ gitprac2/src/gitprac2/main.java
@@ -0,0 +1,14 @@
+package gitprac2;
+
+
+public class main{
+	
+	public static void main(String args[]){ 
+	
+	jump jumpagent = new jump();
+	System.out.println("------start------");
+	jumpagent.jumphalo();
+	System.out.println("------finish------");
+	
+	}
+}
